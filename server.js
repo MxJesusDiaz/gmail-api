580550213a19a972ebded65e174808e544ddd6fc
MxJesusDiaz <mxjesusdiaz99@gmail.com>
2021-06-25 16:33:36 -0500

    Test 2

---
+++ server.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const dotenv = require('dotenv')
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.user,
+        pass: process.env.pass
+    }
+});
+
+let mailOptions = {
+    from: 'testing.yisus@gmail.com',
+    to: 'sergio@cajasmg.com, alejandro@cajasmg.com',
+    subject: 'Funciona C:',
+    text: 'Si funciona ✨🎉'
+}
+
+transporter.sendMail(mailOptions, function(err, data){
+    if(err){
+        console.log('An error has ocurred! :c')
+    } else {
+        console.log('Email has sent successfully!! c:')
+    }
+})
